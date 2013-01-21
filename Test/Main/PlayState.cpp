095b58e4c39e32ba1d268d4bf4f71ea9eaa6aa49
Joe Dupuis <joe.dupuis@anhero.net>
2013-01-21 12:47:36 -0500

    Fixed some include macro from relative to absolute and switch the netbeans project from MainTest to Main.

--- Test/Main/PlayState.cpp
+++ Test/Main/PlayState.cpp
@@ -1,24 +1,11 @@
 #include "PlayState.h"
 
-#include <Core/Entity.h>
-#include <Components/Transform.h>
+#include <BaconBox/Core/Entity.h>
+#include <BaconBox/Components/Transform.h>
 
 namespace BaconBox {
 	PlayState::PlayState(const std::string newName) : State(newName) {
-		Entity *entity = new Entity();
-		
-		Transform *transform = new Transform();
-		entity->addComponent(transform);
-		
-		float f = 30.0f;
-		entity->sendMessage(Transform::ID, Transform::MESSAGE_SET_ROTATION, &f);
-		
-		float ff = 0.0f;
-		entity->sendMessage(Transform::ID, Transform::MESSAGE_GET_ROTATION, &ff);
-		
-		std::cout << "omg " << ff << std::endl;
-		
-		this->add(entity);
+	
 	}
 	
 	void PlayState::update() {
