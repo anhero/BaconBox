5ea2b0eb0ec402d980902259f3a56544a9eee7d0
Joé Dupuis <joe.dupuis@anhero.net>
2013-01-25 01:08:01 -0500

    The getEntity method now return and entity when given a class name on flascc.

--- BaconBox/EntityFactory.cpp
+++ BaconBox/EntityFactory.cpp
@@ -7,21 +7,27 @@
 #endif	
 namespace BaconBox {
 	EntityFactory::EntityFactory(){
-	    
 	}
 	
 	Entity * EntityFactory::getEntity(const std::string & key){
-	#ifdef BB_FLASH_PLATEFORM	
-	AS3::local::var className = AS3::local::internal::new_String(key.c_str());
-	AS3::local::var cc = AS3::local::internal::getlex(className);
-	AS3::local::var mc = AS3::local::internal::construct(cc);
-	
-	
-	AS3::local::var propertyNameAS = AS3::local::internal::new_String("entity");
-	AS3::local::var entityPointerAS3 = AS3::local::internal::getproperty(mc, propertyNameAS);
-	Entity * entityPointer = (Entity*)int_valueOf(entityPointerAS3);
-	return entityPointer;
-	#endif	  
+		#ifdef BB_FLASH_PLATEFORM	
+			AS3::local::var className = AS3::local::internal::new_String(key.c_str());
+			AS3::local::var functionName = AS3::local::internal::new_String("getDefinitionByName");
+			AS3::local::var ns = AS3::local::internal::new_Namespace("flash.utils");
+
+			AS3::local::var functionAS3 = AS3::local::internal::getlex(ns, functionName);
+			AS3::local::var args[1] = {className};
+			AS3::local::var mcClass = AS3::local::internal::call(functionAS3, AS3::local::internal::_null, 1, args);
+			AS3::local::var mc =  AS3::local::internal::construct(mcClass);
+
+			AS3::local::var propertyNameAS = AS3::local::internal::new_String("entity");
+			AS3::local::var entity = AS3::local::internal::getproperty(mc, propertyNameAS);
+
+			propertyNameAS = AS3::local::internal::new_String("swigCPtr");
+			AS3::local::var entityPointerAS3 = AS3::local::internal::getproperty(entity, propertyNameAS);
+			Entity * entityPointer = (Entity*)int_valueOf(entityPointerAS3);
+			return entityPointer;
+		#endif	  
 	}
 }
 
