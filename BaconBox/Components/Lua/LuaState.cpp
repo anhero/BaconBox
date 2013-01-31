f377fb0aea24abdf76c9ad4d0853c801eff68399
Joé Dupuis <joe.dupuis@anhero.net>
2013-01-30 22:17:04 -0500

    It's now possible to register a luaclass in the LuaState component.

--- BaconBox/Components/Lua/LuaState.cpp
+++ BaconBox/Components/Lua/LuaState.cpp
@@ -12,10 +12,21 @@ namespace BaconBox {
 		setName("LuaState");
 	}
 
+	void LuaState::update(){
+		lua_rawgeti(L, LUA_REGISTRYINDEX,table_index);
+		lua_rawgeti(L, LUA_REGISTRYINDEX,update_index);
+		lua_pcall(L, 1, 0, 0);
+	}
+
+	void LuaState::render(){
+
+	}
 
 	void LuaState::setLuaClass(lua_State * L){
-		std::cout << L << std::endl;
-
+		table_index = luaL_ref(L, LUA_REGISTRYINDEX);
+		lua_getfield(L, -1, "update");
+		update_index = luaL_ref(L, LUA_REGISTRYINDEX);
+		this->L = L;
 	}
 
 	void LuaState::receiveMessage(int senderID, int destID, int message, void *data) {
--- BaconBox/Components/Lua/LuaState.h
+++ BaconBox/Components/Lua/LuaState.h
@@ -13,11 +13,16 @@ namespace BaconBox {
 		LuaState();
 
 		void setLuaClass(lua_State * L);
+
+		void update();
+		void render();
 					
 		virtual void receiveMessage(int senderID, int destID, int message, void *data);
 		
 	private:
-
+		int table_index;
+		int update_index;
+		lua_State * L;
 	};
 }
 
