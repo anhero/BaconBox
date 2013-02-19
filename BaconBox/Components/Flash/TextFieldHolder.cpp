fecf829f3f0abc6a24c8f2398746d87b15bdeff5
Joe Dupuis <joe.dupuis@anhero.net>
2013-02-19 18:38:07 -0500

    Created a TextFieldHolder class similar to the MovieClipHolder class, it would be nice to merge them in the future.

---
+++ BaconBox/Components/Flash/TextFieldHolder.cpp
@@ -0,0 +1,46 @@
+#include "TextFieldHolder.h"
+#include "BaconBox/Core/Entity.h"
+#include "BaconBox/Components/Transform.h"
+#include "BaconBox/Core/IDManager.h"
+
+
+#include "BaconBox/Helper/Flash/FlashHelper.h"
+#include "BaconBox/Console.h"
+#include "BaconBox/Helper/Vector2ChangedData.h"
+
+namespace BaconBox {
+
+	BB_ID_IMPL(TextFieldHolder);
+	
+
+	TextFieldHolder::TextFieldHolder(AS3::local::var aMC) : Component(){
+		setMovieClip(aMC);
+	}
+
+	void TextFieldHolder::setMovieClip(AS3::local::var aMC){
+		mc = aMC;
+	}
+	
+	AS3::local::var TextFieldHolder::getMovieClip(){
+		return mc;
+	}
+
+	void TextFieldHolder::receiveMessage(int senderID, int destID, int message, void *data){
+		 if(destID != Entity::BROADCAST && destID != TextFieldHolder::ID ) return;
+
+		 if(senderID == Transform::ID){
+		 	if(message == Transform::MESSAGE_POSITION_CHANGED)	{
+				Vector2ChangedData * pos = (reinterpret_cast<Vector2ChangedData *>(data));
+		 	}	
+		 	else if(message == Transform::MESSAGE_ROTATION_CHANGED){
+		 		ValueChangedData<float>* rotation= reinterpret_cast<ValueChangedData<float>*>(data);
+
+		 	}
+		 	else if(message == Transform::MESSAGE_SCALE_CHANGED){
+		 		Vector2ChangedData * scale = (reinterpret_cast<Vector2ChangedData *>(data));
+		 	}	
+		 }
+
+	}
+    
+}
---
+++ BaconBox/Components/Flash/TextFieldHolder.h
@@ -0,0 +1,34 @@
+/**
+ * @file
+ */
+#ifndef BB_TEXTFIELDHOLDER_H
+#define BB_TEXTFIELDHOLDER_H
+
+#include "../../Core/Component.h"
+
+#include <AS3/AS3.h>
+#include <AS3/AS3++.h>
+
+namespace BaconBox {
+	/**
+	 *
+	 */
+	class TextFieldHolder  : public Component{
+    public:
+    	
+    	BB_ID_HEADER;
+    	
+        TextFieldHolder(AS3::local::var aMC);
+		void setMovieClip(AS3::local::var aMC);
+		AS3::local::var getMovieClip();
+		virtual void receiveMessage(int senderID, int destID, int message, void *data);
+		
+    private:
+		AS3::local::var mc;
+		AS3::local::var textField;
+		AS3::local::var textFormat;
+
+	};
+}
+
+#endif // BB_MOVIECLIPCOMPONENT_H
