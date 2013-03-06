7e732715daa172e45c24e1e709d4b18d65e7891b
Joe Dupuis <joe.dupuis@anhero.net>
2013-03-06 17:56:55 -0500

    Created a Matrix class.

---
+++ BaconBox/Matrix.h
@@ -0,0 +1,177 @@
+/**
+ * @file
+ * @ingroup Math
+ */
+#ifndef BB_MATRIX_H
+#define BB_MATRIX_H
+
+#include <cmath>
+
+#include "BaconBox/Helper/MathHelper.h"
+#include "BaconBox/Helper/Serialization/Array.h"
+#include "BaconBox/Helper/Serialization/Object.h"
+
+#include "BaconBox/Helper/Serialization/Value.h"
+#include "BaconBox/Helper/Serialization/DefaultSerializer.h"
+#include "BaconBox/Helper/Serialization/Serializer.h"
+
+namespace BaconBox {
+    template <typename T>
+    struct Matrix{
+	T a;
+	T b;
+	T c;
+	T d;
+	T tx;
+	T ty;
+	Matrix():a(1),b(0),c(0),d(1),tx(0),ty(0){}
+	Matrix(T a, T b, T c, T d, T tx, T ty):a(a),b(b),c(c),d(d),tx(tx),ty(ty){}
+	Matrix(const Matrix &src);
+	
+	Matrix<T> &operator=(const Matrix<T> &src) {
+		a = src.a;
+		b = src.b;
+		c = src.c;
+		d = src.d;
+		tx = src.tx;
+		ty = src.ty;
+		return *this;
+	}
+
+	
+	Matrix<T> operator*(const Matrix<T> & m) const{
+	    return Matrix<T>(a*m.a + c*m.b, 
+			     b*m.a + d*m.b, 
+			     a*m.c + c*m.d,
+			     b*m.c + d*m.d,
+			     a*m.tx+ c*m.ty +tx,
+			     b*m.tx+ d*m.ty +ty);
+	}
+	
+
+	
+	Matrix<T> &operator*=(const Matrix<T>& m) {
+		(*this) = (*this) * m;
+		return (*this);
+	}
+	
+	void concat(const Matrix<T> & m){
+	    (*this) = m * (*this);
+	}
+	
+	void translate(T x, T y){
+	    tx += x;
+	    ty += y;
+	}
+	
+	void scale(T x, T y){
+	    a *= x;
+	    d *= d;
+	}
+	
+	void rotate(T angle){
+	    angle *= -1 * MathHelper::AngleConvert<T>::DEGREES_TO_RADIANS; 
+	    T cosValue = std::cos(angle);
+	    T sinValue = std::sin(angle);
+	    (*this) = Matrix(cosValue, -sinValue, sinValue, cosValue,0,0) * (*this);
+	
+	}
+	
+	void serialize(Value &node, bool setName = true) const {
+			if (setName) {
+				node.setName("Matrix2");
+			}
+			node["a"].setDouble(static_cast<double>(a));
+			node["b"].setDouble(static_cast<double>(b));
+			node["c"].setDouble(static_cast<double>(c));
+			node["d"].setDouble(static_cast<double>(d));
+			node["tx"].setDouble(static_cast<double>(tx));
+			node["ty"].setDouble(static_cast<double>(ty));
+		}
+
+		/**
+		 * Deserializes the instance from a Value.
+		 * @param node Value to deserialize the value from.
+		 * @see BaconBox::DefaultSerializer
+		 */
+		bool deserialize(const Value &node) {
+			bool result = true;
+			if (node.getArray().size() == 6) {
+				a = static_cast<T>(node.getArray()[0].getDouble());
+				b = static_cast<T>(node.getArray()[1].getDouble());
+				c = static_cast<T>(node.getArray()[2].getDouble());
+				d = static_cast<T>(node.getArray()[3].getDouble());
+				tx = static_cast<T>(node.getArray()[4].getDouble());
+				ty = static_cast<T>(node.getArray()[5].getDouble());
+			} else {
+				Object::const_iterator itA = node.getObject().find("a");
+				Object::const_iterator itB = node.getObject().find("b");
+				Object::const_iterator itC = node.getObject().find("c");
+				Object::const_iterator itD = node.getObject().find("d");
+				Object::const_iterator itTX = node.getObject().find("tx");
+				Object::const_iterator itTY = node.getObject().find("ty");
+				
+
+				// We make sure the value contains the "x" and "y" values.
+				if (itA != node.getObject().end() &&
+				    itB != node.getObject().end() &&
+				    itC != node.getObject().end() &&
+				    itD != node.getObject().end() &&
+				    itTX != node.getObject().end() &&
+				    itTY != node.getObject().end() &&
+				    itA->second.isNumeric() && itB->second.isNumeric() &&
+				    itC->second.isNumeric() && itD->second.isNumeric() &&
+				    itTX->second.isNumeric() && itTY->second.isNumeric()) {
+
+					a = static_cast<T>(itA->second.getDouble());
+					b = static_cast<T>(itB->second.getDouble());
+					c = static_cast<T>(itC->second.getDouble());
+					d = static_cast<T>(itD->second.getDouble());
+					tx = static_cast<T>(itTX->second.getDouble());
+					ty = static_cast<T>(itTY->second.getDouble());
+
+				} else {
+					result = false;
+				}
+			}
+
+			return result;
+		}
+
+		
+		static bool isValidValue(const Value &node) {
+			Object::const_iterator itA = node.getObject().find("a");
+			Object::const_iterator itB = node.getObject().find("b");
+			Object::const_iterator itC = node.getObject().find("c");
+			Object::const_iterator itD = node.getObject().find("d");
+			Object::const_iterator itTX = node.getObject().find("tx");
+			Object::const_iterator itTY = node.getObject().find("ty");
+
+			return node.getArray().size() == 6 || (itA != node.getObject().end() && itB != node.getObject().end() &&
+								itC != node.getObject().end() && itD != node.getObject().end() &&
+								itTX != node.getObject().end() && itTY != node.getObject().end() &&
+								itA->second.isNumeric() && itB->second.isNumeric() &&
+								itC->second.isNumeric() && itD->second.isNumeric() &&
+								itTX->second.isNumeric() && itTY->second.isNumeric());
+
+		}
+	
+	
+    };
+    typedef Matrix<float> Matrix2;
+    
+    template <typename T>
+	Matrix<T>::Matrix(const Matrix<T> &src) : a(src.a), b(src.b), c(src.c), d(src.d), tx(src.tx), ty(src.ty) {}
+    
+    
+    template <typename T>
+	std::ostream &operator<<(std::ostream &output, const Matrix<T>& m) {
+		Value tmpValue;
+		DefaultSerializer::serialize(m, tmpValue);
+		DefaultSerializer::getDefaultSerializer().writeToStream(output, tmpValue);
+		return output;
+	}
+    
+}
+
+#endif
