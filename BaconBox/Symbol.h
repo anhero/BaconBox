9afa45d990399f40be73e71ae168f41f9c230143
Joe Dupuis <joe.dupuis@anhero.net>
2013-03-12 17:59:49 -0400

    Created the Symbol struct.

---
+++ BaconBox/Symbol.h
@@ -0,0 +1,16 @@
+
+
+#ifndef BB_SYMBOL_H
+#define BB_SYMBOL_H
+
+namespace BaconBox {
+    struct SymbolPart;
+    struct Symbol{
+	std::map<int,SymbolPart> parts;
+    };
+    
+	
+    
+}
+
+#endif
