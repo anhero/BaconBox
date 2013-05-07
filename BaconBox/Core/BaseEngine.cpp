3f70da80508632db737864754e5aa11c5e5a8b76
Joe Dupuis <joe.dupuis@anhero.net>
2013-05-07 13:39:18 -0400

    Engine::application won't crash if you give a null argv pointer.

--- BaconBox/Core/BaseEngine.cpp
+++ BaconBox/Core/BaseEngine.cpp
@@ -36,98 +36,98 @@ namespace BaconBox {
 	void BaseEngine::application(int argc, char *argv[], const std::string &name) {
 		this->argc = argc;
 		this->argv = argv;
-		this->applicationPath = dirname(argv[0]);
+		if(argv)this->applicationPath = dirname(argv[0]);
 		this->applicationName = name;
 	}
-	
+
 	State *BaseEngine::addState(State *newState) {
 		if (newState) {
 			if (this->states.empty()) {
 				assert(!this->currentState);
 				this->nextState = newState;
 			}
-			
+
 			this->states.insert(std::pair<std::string, State *>(newState->getName(), newState));
 		}
-		
+
 		return newState;
 	}
-	
+
 	State *BaseEngine:: getState(const std::string &name){
 	    std::map<std::string, State *>::iterator i = this->states.find(name);
 	    return i->second;
 	}
 
-	
+
 	void BaseEngine::removeState(const std::string &name) {
 		std::map<std::string, State *>::iterator toDelete = this->states.find(name);
-		
+
 		if (this->nextState != toDelete->second) {
 			if (toDelete->second) {
 				delete toDelete->second;
 			}
-			
+
 			this->states.erase(toDelete);
 		}
 		this->currentState = NULL;
 	}
-	
+
 	void BaseEngine::removeAllStates() {
 	    std::list<std::string> statesNames;
 		for(std::map<std::string, State *>::iterator i= this->states.begin();
 			i!= this->states.end(); i++){
 		    statesNames.push_back(i->first);
 		}
-	    
+
 		for(std::list<std::string>::iterator i= statesNames.begin();
 			i!= statesNames.end(); i++){
 		    removeState(*i);
-		}	
+		}
 	}
-	
+
 	State *BaseEngine::playState(const std::string &name) {
 		// We make sure the state asked for exists.
 		std::map<std::string, State *>::iterator it = this->states.find(name);
-		
+
 		if (it != this->states.end()) {
 			this->nextState = it->second;
-			
+
 		} else {
 			Console::println("State \"" + name +
 			                 "\" doesn't exist so it cannot be played.");
 		}
-		
+
 		return this->nextState;
 	}
-	
+
 	State *BaseEngine::getCurrentState() {
 		return this->currentState;
 	}
-	
+
 	unsigned int BaseEngine::getMinFps() {
 		return this->minFps;
 	}
-	
+
 	double BaseEngine::getUpdatesPerSecond() {
 		assert(this->updateDelay);
 		return 1.0 / this->updateDelay;
 	}
-	
+
 	double BaseEngine::getUpdateDelay() {
 		return this->updateDelay;
 	}
-	
+
 	void BaseEngine::setMinFps(unsigned int newMinFps) {
 		this->minFps = newMinFps;
 	}
-	
+
 	void BaseEngine::setUpdatesPerSecond(double updatesPerSecond) {
 		if (updatesPerSecond) {
 			// Calculate the update delay.
 			this->updateDelay = 1.0 / static_cast<double>(updatesPerSecond);
 		}
 	}
-	
+
 	void BaseEngine::switchToNextState(){
 	    if (this->nextState) {
 					// If the next state is the first state the engine is
@@ -136,41 +136,41 @@ namespace BaconBox {
 					if (this->currentState) {
 						this->currentState->internalOnLoseFocus();
 					}
-					
+
 					// We set the next state as the current state.
 					this->currentState = this->nextState;
 					// We call the onGetFocus method.
 					this->currentState->internalOnGetFocus();
-					
+
 					this->nextState = NULL;
 				}
-				
+
 	}
 
-	
+
 	void BaseEngine::pulse() {
 		// We make sure the pointer to the current state is valid.
 		if (this->currentState || this->nextState) {
 			TimeHelper::getInstance().refreshTime();
-			
+
 			// We update the time from TimeHelper.
 			if (!this->nextUpdate) {
 				this->nextUpdate = TimeHelper::getInstance().getSinceStartComplete();
 			}
-			
+
 			this->loops = 0;
-			
+
 			while (TimeHelper::getInstance().getSinceStartComplete() > this->nextUpdate &&
 			       this->loops < this->minFps) {
 				// We refresh the time.
 				TimeHelper::getInstance().refreshTime();
-				
+
 				// We call the focus methods if needed.
 				switchToNextState();
-					
+
 				// We update the current state.
 				this->currentState->internalUpdate();
-				
+
 				this->renderedSinceLastUpdate = false;
 				// We update the input manager.
 				InputManager::getInstance().update();
@@ -180,7 +180,7 @@ namespace BaconBox {
 				this->lastUpdate = TimeHelper::getInstance().getSinceStartComplete();
 				++this->loops;
 			}
-			
+
 			if (!this->renderedSinceLastUpdate) {
 			    if(! this->currentState && this->nextState){
 				switchToNextState();
@@ -191,19 +191,19 @@ namespace BaconBox {
 				this->bufferSwapped = false;
 				this->lastRender = TimeHelper::getInstance().getSinceStartComplete();
 			}
-			
+
 			if (static_cast<AudioEngine *>(this->soundEngine) != static_cast<AudioEngine *>(this->musicEngine)) {
 				this->soundEngine->update();
 			}
-			
+
 			this->musicEngine->update();
 		}
-		
+
 		if (this->needsExit) {
 			exit(this->tmpExitCode);
 		}
 	}
-	
+
 	void BaseEngine::initializeEngine(unsigned int resolutionWidth,
 	                              unsigned int resolutionHeight,
 	                              float contextWidth,
@@ -212,63 +212,63 @@ namespace BaconBox {
 		TimeHelper::getInstance();
 		InputManager::getInstance();
 		Engine::onInitialize.shoot(resolutionWidth, resolutionHeight, contextWidth, contextHeight);
-		
+
 		this->graphicDriver->initializeGraphicDriver();
 	}
-	
+
 	double BaseEngine::getSinceLastUpdate() {
 		return (this->lastUpdate) ? (TimeHelper::getInstance().getSinceStartComplete() - this->lastUpdate) : (this->lastUpdate);
 	}
-	
+
 	double BaseEngine::getSinceLastRender() {
 		return (this->lastRender) ? (TimeHelper::getInstance().getSinceStartComplete() - this->lastRender) : (this->lastRender);
 	}
-	
+
 	bool BaseEngine::isBufferSwapped() {
 		return this->bufferSwapped;
 	}
-	
+
 	void BaseEngine::setBufferSwapped() {
 		this->bufferSwapped = true;
 	}
-	
+
 	void BaseEngine::exitApplication(int exitCode) {
 		this->needsExit = true;
 		this->tmpExitCode = exitCode;
 	}
-	
+
 	void BaseEngine::showMainWindow() {
 		MainWindow::getInstance().show();
 	}
-	
+
 	const std::string &BaseEngine::getApplicationPath() {
 		return this->applicationPath;
 	}
-	
+
 	const std::string &BaseEngine::getApplicationName() {
 		return this->applicationName;
 	}
-	
+
 	int &BaseEngine::getApplicationArgc() {
 		return this->argc;
 	}
-	
+
 	char **BaseEngine::getApplicationArgv() {
 		return this->argv;
 	}
-	
+
 	MainWindow &BaseEngine::getMainWindow() {
 		return *this->mainWindow;
 	}
-	
+
 	GraphicDriver &BaseEngine::getGraphicDriver() {
 		return *this->graphicDriver;
 	}
-	
+
 	SoundEngine &BaseEngine::getSoundEngine() {
 		return *this->soundEngine;
 	}
-	
+
 	MusicEngine &BaseEngine::getMusicEngine() {
 		return *this->musicEngine;
 	}
@@ -279,39 +279,39 @@ namespace BaconBox {
 	tmpExitCode(0), renderedSinceLastUpdate(true), applicationPath(),
 	applicationName(Engine::DEFAULT_APPLICATION_NAME), mainWindow(NULL),
 	graphicDriver(NULL), soundEngine(NULL), musicEngine(NULL) {
-		
+
 		mainWindow = BB_MAIN_WINDOW_IMPL;
 		graphicDriver = BB_GRAPHIC_DRIVER_IMPL;
 		soundEngine = BB_SOUND_ENGINE_IMPL;
 		musicEngine = BB_MUSIC_ENGINE_IMPL;
 	}
-	
+
 	BaseEngine::~BaseEngine() {
 		// We delete the states.
 		std::for_each(states.begin(), states.end(), DeletePointerFromPair());
-		
+
 		// We unload the resources.
 		ResourceManager::unloadAll();
-		
+
 		// We unload the audio engines.
 		if (static_cast<AudioEngine *>(musicEngine) == static_cast<AudioEngine *>(soundEngine)) {
 			delete musicEngine;
-			
+
 		} else {
 			if (musicEngine) {
 				delete musicEngine;
 			}
-			
+
 			if (soundEngine) {
 				delete soundEngine;
 			}
 		}
-		
+
 		// We unload the graphic driver;
 		if (graphicDriver) {
 			delete graphicDriver;
 		}
-		
+
 		// We unload the main window;
 		if (mainWindow) {
 			delete mainWindow;
