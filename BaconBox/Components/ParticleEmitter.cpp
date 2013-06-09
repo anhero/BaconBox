69645d63dfce14d3eef6572b20b595821484e437
Adam Labranche <adam.labranche@anhero.net>
2013-06-09 15:29:54 -0400

    Continued to work on the ParticleEmitter class.

--- BaconBox/Components/ParticleEmitter.cpp
+++ BaconBox/Components/ParticleEmitter.cpp
@@ -3,10 +3,10 @@
 namespace BaconBox {
 	BB_ID_IMPL(ParticleEmitter);
 	
-	ParticleEmitter::ParticleEmitter() : Component(), Emitter(), enabled(false) {
+	ParticleEmitter::ParticleEmitter() : Component(), Emitter(), emitting(false), infiniteEmission(true), minEmissionTime(0.0), maxEmissionTime(0.0), stopwatch(), emissionRate(10.0) {
 	}
 	
-	ParticleEmitter::ParticleEmitter(const ParticleEmitter &src) : Component(src), Emitter(src), enabled(src.enabled) {
+	ParticleEmitter::ParticleEmitter(const ParticleEmitter &src) : Component(src), Emitter(src), emitting(src.emitting), infiniteEmission(src.infiniteEmission), minEmissionTime(src.minEmissionTime), maxEmissionTime(src.maxEmissionTime), stopwatch(src.stopwatch), emissionRate(src.emissionRate) {
 	}
 	
 	ParticleEmitter::~ParticleEmitter() {
@@ -17,7 +17,12 @@ namespace BaconBox {
 		this->Emitter::operator=(src);
 		
 		if (this != &src) {
-			this->enabled = src.enabled;
+			this->emitting = src.emitting;
+			this->infiniteEmission = src.infiniteEmission;
+			this->minEmissionTime = src.minEmissionTime;
+			this->maxEmissionTime = src.maxEmissionTime;
+			this->stopwatch = src.stopwatch;
+			this->emissionRate = src.emissionRate;
 		}
 		
 		return *this;
@@ -28,8 +33,76 @@ namespace BaconBox {
 	}
 	
 	void ParticleEmitter::update() {
-		if (this->enabled) {
+		if (this->emitting) {
 		}
+	}
+	
+	bool ParticleEmitter::isEmitting() const {
+		return this->emitting;
+	}
+	
+	void ParticleEmitter::start() {
+		this->emitting = true;
+		
+		if (!this->isInfiniteEmission()) {
+			this->stopwatch.start();
+		}
+	}
+	
+	void ParticleEmitter::pause() {
+		this->emitting = false;
+		
+		if (!this->isInfiniteEmission()) {
+			this->stopwatch.pause();
+		}
+	}
+	
+	void ParticleEmitter::stop() {
+		this->emitting = false;
+		
+		if (!this->isInfiniteEmission()) {
+			this->stopwatch.stop();
+		}
+	}
+	
+	bool ParticleEmitter::isInfiniteEmission() const {
+		return this->infiniteEmission;
+	}
+	
+	void ParticleEmitter::setInfiniteEmission(bool newInfiniteEmission) {
+		if (this->infiniteEmission != newInfiniteEmission) {
+			this->infiniteEmission = newInfiniteEmission;
+			
+			if (this->infiniteEmission) {
+				this->stopwatch.stop();
+			} else if (this->emitting) {
+				this->stopwatch.start();
+			}
+		}
+	}
+	
+	double ParticleEmitter::getMinEmissionTime() const {
+		return this->minEmissionTime;
+	}
+	
+	void ParticleEmitter::setMinEmissionTime(double newMinEmissionTime) {
+		this->minEmissionTime = newMinEmissionTime;
+	}
+	
+	double ParticleEmitter::getMaxEmissionTime() const {
+		return this->maxEmissionTime;
+	}
+	
+	void ParticleEmitter::setMaxEmissionTime(double newMaxEmissionTime) {
+		this->maxEmissionTime = newMaxEmissionTime;
+	}
+	
+	double ParticleEmitter::getEmissionRate() const {
+		return this->emissionRate;
+	}
+	
+	void ParticleEmitter::setEmissionRate(double newEmissionRate) {
+		this->emissionRate = newEmissionRate;
 	}
 	
 	void ParticleEmitter::emitParticle(double lifetime, float force, float angle, float angularVelocity, int rotationDirection) {
--- BaconBox/Components/ParticleEmitter.h
+++ BaconBox/Components/ParticleEmitter.h
@@ -3,6 +3,7 @@
 
 #include "BaconBox/Display/Emitter.h"
 #include "BaconBox/Core/Component.h"
+#include "BaconBox/Helper/Stopwatch.h"
 
 namespace BaconBox {
 	class ParticleEmitter : public Component, public Emitter {
@@ -23,10 +24,50 @@ namespace BaconBox {
 		
 		using Emitter::emitParticle;
 		
+		bool isEmitting() const;
+		
+		void start();
+		void pause();
+		void stop();
+		
+		bool isInfiniteEmission() const;
+		void setInfiniteEmission(bool newInfiniteEmission);
+		
+		double getMinEmissionTime() const;
+		void setMinEmissionTime(double newMinEmissionTime);
+		
+		double getMaxEmissionTime() const;
+		void setMaxEmissionTime(double newMaxEmissionTime);
+		
+		double getEmissionRate() const;
+		void setEmissionRate(double newEmissionRate);
+		
 	protected:
 		void emitParticle(double lifetime, float force, float angle, float angularVelocity, int rotationDirection);
 	private:
-		bool enabled;
+		bool emitting;
+		
+		/**
+		 * Determines whether or not the emitter emits particles infinitely
+		 * when started.
+		 */
+		bool infiniteEmission;
+		
+		/**
+		 * Minimum number of seconds the emitter will emit particles before
+		 * stopping. Ignored if infiniteEmission is set to true.
+		 */
+		double minEmissionTime;
+		/**
+		 * Maximum number of seconds the emitter will emit particles before
+		 * stopping. Ignored if infiniteEmission is set to true.
+		 */
+		double maxEmissionTime;
+
+		Stopwatch stopwatch;
+		
+		/// Number of particles spawned per second.
+		double emissionRate;
 	};
 }
 
