47f1babca98f1bdc982102e4b48ecadf50b694ef
Joé Dupuis <joe.dupuis@anhero.net>
2013-09-23 12:26:17 -0400

    Print webdav server ip address

--- BaconBox/Display/Window/ios/BaconBoxAppAppDelegate.m
+++ BaconBox/Display/Window/ios/BaconBoxAppAppDelegate.m
@@ -11,7 +11,8 @@
 #import "DDTTYLogger.h"
 #import "HTTPServer.h"
 #import "DAVConnection.h"
-
+#import <ifaddrs.h>
+#import <arpa/inet.h>
 
 static const int ddLogLevel = LOG_LEVEL_VERBOSE;
 
@@ -61,7 +62,10 @@ static BaconBoxAppViewController *baconBoxViewController = nil;
 	DDLogInfo(@"Setting document root: %@", webPath);
 	[httpServer setDocumentRoot:webPath];
     [self startServer];
+	NSString * ip = [self getIPAddress];
+	std::cout << "HTTP SERVER IP ADDRESS:  " << [ip cStringUsingEncoding:NSUTF8StringEncoding] << std::endl;
 }
+
 
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
@@ -90,6 +94,35 @@ static BaconBoxAppViewController *baconBoxViewController = nil;
 	 Use this method to pause ongoing tasks, disable timers, and throttle down OpenGL ES frame rates. Games should use this method to pause the game.
 	 */
 	[self.viewController stopAnimation];
+}
+
+
+// Get IP Address
+- (NSString *)getIPAddress {
+    NSString *address = @"error";
+    struct ifaddrs *interfaces = NULL;
+    struct ifaddrs *temp_addr = NULL;
+    int success = 0;
+    // retrieve the current interfaces - returns 0 on success
+    success = getifaddrs(&interfaces);
+    if (success == 0) {
+        // Loop through linked list of interfaces
+        temp_addr = interfaces;
+        while(temp_addr != NULL) {
+            if(temp_addr->ifa_addr->sa_family == AF_INET) {
+                // Check if interface is en0 which is the wifi connection on the iPhone
+                if([[NSString stringWithUTF8String:temp_addr->ifa_name] isEqualToString:@"en0"]) {
+                    // Get NSString from C String
+                    address = [NSString stringWithUTF8String:inet_ntoa(((struct sockaddr_in *)temp_addr->ifa_addr)->sin_addr)];
+                }
+            }
+            temp_addr = temp_addr->ifa_next;
+        }
+    }
+    // Free memory
+    freeifaddrs(interfaces);
+    return address;
+	
 }
 
 - (void)applicationWillTerminate:(UIApplication *)application
