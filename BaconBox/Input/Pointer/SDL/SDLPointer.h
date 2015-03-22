27e063a17461ae470f4a7ba4b685c3c104b34ade
Samuel Dionne-Riel <samuel@dionne-riel.com>
2015-03-22 16:48:11 -0400

    Draft touch screen handling in SDL.

--- BaconBox/Input/Pointer/SDL/SDLPointer.h
+++ BaconBox/Input/Pointer/SDL/SDLPointer.h
@@ -6,6 +6,9 @@
 #define BB_SDL_POINTER_H
 
 #include "BaconBox/Input/Pointer/Pointer.h"
+#include <SDL2/SDL_touch.h>
+#include <map>
+#include <set>
 
 namespace BaconBox {
 	/**
@@ -29,6 +32,27 @@ namespace BaconBox {
 		 * Updates the mouse's buttons state from SDL's mouse state.
 		 */
 		void updateDevice();
+
+		/**
+		 * Whether we have to handle a touch screen
+		 */
+		bool hasTouchscreen;
+
+		/**
+		 * Touchscreen specifics
+		 */
+		void updateTouchScreen();
+
+		/**
+		 * List of SDL IDs in relationship of BaconBox pointers, for the last
+		 * finger touches. Used to track fingers.
+		 */
+		std::map<SDL_FingerID, unsigned int> touchCache;
+
+		/**
+		 * List of available BaconBox pointer IDs
+		 */
+		std::set<unsigned int> availIDs;
 	};
 }
 
