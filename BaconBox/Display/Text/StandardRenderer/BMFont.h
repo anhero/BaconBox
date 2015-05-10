dde0eb4c4000ca96ef56400b8eb986d473cbbeae
Samuel Dionne-Riel <samuel@dionne-riel.com>
2015-05-10 17:46:39 -0400

    Implemented loading BMFont from FileSystem + refactored a bit.

--- BaconBox/Display/Text/StandardRenderer/BMFont.h
+++ BaconBox/Display/Text/StandardRenderer/BMFont.h
@@ -7,36 +7,28 @@
 
 #include "BaconBox/Display/Text/StandardRenderer/TextureFont.h"
 
-
 namespace BaconBox {
-
 	class BMFont : public TextureFont {
 	public:
 		BMFont();
 		BMFont(const std::string &newName);
-		
-		
-		
 		int getKerning(Char32 leftUnicodeValue, Char32 rightUnicodeValue);
-
-		
 		/**
 		 * Return the glyph specified by the unicode value.
 		 * The size of the font should be set before calling
 		 * this function.
 		 */
 		TextureGlyphInformation *getGlyphInformation(Char32 unicodeValue);
-
 		/**
 		 * Set the font pixel size.
 		 */
 		void setPixelSize(int pixelSize);
-		
+		/// Detects the format and loads the font from the file.
 		void loadFontFile(const std::string &filename);
-
-
-	private:
-	    
+	protected:
+		void loadFontFileFromXML(const std::string &filename);
+		void loadFontFileFromTXT(const std::string &filename);
+		void loadFontFileFromBIN(const std::string &filename);
 	};
 }
 
