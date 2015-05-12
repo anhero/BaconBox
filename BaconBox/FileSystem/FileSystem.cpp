66660a9870360af2e6ab8b7f0a90fb29f4d59428
Samuel Dionne-Riel <samuel@dionne-riel.com>
2015-05-12 16:56:09 -0400

    Mounting of write paths now create the write path.

--- BaconBox/FileSystem/FileSystem.cpp
+++ BaconBox/FileSystem/FileSystem.cpp
@@ -3,6 +3,8 @@
 #include "BaconBox/Core/Engine.h"
 #include "BaconBox/Console.h"
 
+#include "BaconBox/FileSystem/SystemFS.h"
+
 // TODO : Move to configuration.
 #define BB_WITH_PHYSFS
 
@@ -49,18 +51,28 @@ bool FileSystem::mount(const std::string& what, const std::string& where, const 
 bool FileSystem::_mount(const std::string& what, const std::string& where, const bool write, const bool append) {
 #ifdef BB_WITH_PHYSFS
 	// TODO : Error handling
-	if (!(bool)PHYSFS_mount(what.c_str(), where.c_str(), append)) {
-		PRLN("Mounting with PHYSFS_mount failed.");
-		PRLN("Reason: '" << PHYSFS_getLastError() << "'");
-		return false;
-	}
 	if (write) {
+		if (!SystemFS::isDirectory(what)) {
+			if (SystemFS::exists(what)) {
+				PRLN("Mounting for write failed; `" << what << "` exists, but is not a directory.");
+				return false;
+			}
+			if (!SystemFS::createDirectoryTree(what)) {
+				PRLN("Mounting for write failed; `" << what << "` could not create directory tree.");
+				return false;
+			}
+		}
 		if (PHYSFS_setWriteDir(what.c_str()) == 0) {
 			PRLN("Mounting for write failed; PHYSFS_setWriteDir failed.");
 			PRLN("Reason: '" << PHYSFS_getLastError() << "'");
 			return false;
 		}
 		PhysFSFile::setWriteMount(where);
+	}
+	if (!(bool)PHYSFS_mount(what.c_str(), where.c_str(), append)) {
+		PRLN("Mounting `" << what << "` with PHYSFS_mount failed.");
+		PRLN("Reason: '" << PHYSFS_getLastError() << "'");
+		return false;
 	}
 	return true;
 #endif
