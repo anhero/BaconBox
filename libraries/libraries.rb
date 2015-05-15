06669058ec6f36a9c7f8f24bceaebf4afa12020c
Samuel Dionne-Riel <samuel@dionne-riel.com>
2015-05-15 12:37:10 -0400

    Using the new $PLATFORM global, cleaned up libraries.rb
     > Removed a useless long-building library from OSX.

--- libraries/libraries.rb
+++ libraries/libraries.rb
@@ -1,16 +1,25 @@
-if $libbuildtool_params.platform_name != "Android"
+puts ":: BaconBox libraries.rb ::"
+puts "   Debug information."
+puts "   Platform : #{$PLATFORM}"
+
+
+if $PLATFORM != "Android"
 build 'zlib.desc'
 end
 build 'libpng.desc' #if $libbuildtool_params.platform_name != "Flash"
 build 'lua.desc'
 build 'jsonbox.desc'
 
-if $libbuildtool_params.platform_name != "Android" and $libbuildtool_params.platform_name != "Flash"
+if $PLATFORM != "Android" and $PLATFORM != "Flash"
 	build 'libogg.desc'
 	build 'libvorbis.desc'
 	build 'SDL2.desc'
 	build 'SDL2_mixer.desc'
-	build 'glew.desc'
+
+	# We are not using glew on Darwin.
+	unless $PLATFORM == "Darwin"
+		build 'glew.desc'
+	end
 end
 
 build 'physicsfs.desc'
