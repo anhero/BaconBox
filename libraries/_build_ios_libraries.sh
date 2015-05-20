ce7b55805bb073cc2f46e5ba22dd37a4e3715ebd
Samuel Dionne-Riel <samuel@dionne-riel.com>
2015-05-20 15:43:22 -0400

    Updated iOS libraries build script for sim + SDK 8.3

--- libraries/_build_ios_libraries.sh
+++ libraries/_build_ios_libraries.sh
@@ -16,16 +16,20 @@ if [[  "${1-}" == "--delete"
 	@ rm -rf iOS;
 fi
 
+VERSION=8.3
+
 # We need to --rebuild all the calls since the current libbuildtool will
 # think it has already built the targets since the built status is global
 # and not by platform.
 
 # First, build the simulator libraries
-@ libbuildtool --rebuild -p IOS -- --sdk=iPhoneSimulator8.1 --arch=x86_64
+for arch in i386 x86_64; do
+	@ libbuildtool --rebuild -p IOS -- --sdk=iPhonesimulator${VERSION} --arch=$arch
+done
 
 # Then, for each device architectures
 for arch in armv7 armv7s arm64; do
-	@ libbuildtool --rebuild -p IOS -- --sdk=iPhoneOS8.1 --arch=$arch
+	@ libbuildtool --rebuild -p IOS -- --sdk=iPhoneOS${VERSION} --arch=$arch
 done
 
 # Then lipofy stuff
